ba90a9dad8b32f3c4929effbc309f793e2583f8e
heinebold <wikidev@matthias-brueckner.de>
2020-02-10 09:40:56 +0100

    Enable populating groups from Access Token
    Add and implement the PluggableAuthPopulateGroups endpoint.
    
    Change-Id: I62978fc2166582a611be72575e7dd1c683853194

--- src/OpenIDConnect.php
+++ src/OpenIDConnect.php
@@ -33,6 +33,8 @@ class OpenIDConnect extends PluggableAuth {
 
 	const OIDC_SUBJECT_SESSION_KEY = 'OpenIDConnectSubject';
 	const OIDC_ISSUER_SESSION_KEY = 'OpenIDConnectIssuer';
+	const OIDC_ACCESSTOKEN_SESSION_KEY = 'OpenIDConnectAccessToken';
+	const OIDC_GROUP_PREFIX = 'oidc_';
 
 	/**
 	 * @since 1.0
@@ -162,6 +164,12 @@ class OpenIDConnect extends PluggableAuth {
 				$this->subject = $oidc->requestUserInfo( 'sub' );
 				$this->issuer = $oidc->getProviderURL();
 
+				$authManager = Authmanager::singleton();
+				$authManager->setAuthenticationSessionData(
+					self::OIDC_ACCESSTOKEN_SESSION_KEY,
+					$oidc->getAccessTokenPayload()
+				);
+
 				$username = $this->getName( $this->subject, $this->issuer );
 				if ( !is_null( $username ) ) {
 					return true;
@@ -246,6 +254,116 @@ class OpenIDConnect extends PluggableAuth {
 				'user_id' => $id
 			], __METHOD__
 		);
+	}
+
+	/**
+	 * Will populate the groups for this user with configurable properties from the access token,
+	 * if one is available for the user.
+	 * Groups will be prefixed with 'oidc_' so the plugin is able to remove them if necessary, i.e.
+	 * when a different access token is used at some other time that contains different groups.
+	 *
+	 * @param User $user
+	 */
+	public static function populateGroups( User $user ) {
+		$old_oidc_groups = array_unique( array_filter( $user->getGroups(), function ( $group ) {
+			return strpos( $group, self::OIDC_GROUP_PREFIX ) === 0;
+		} ) );
+		$new_oidc_groups = self::getOIDCGroups( $user );
+		foreach ( array_diff( $old_oidc_groups, $new_oidc_groups ) as $group_to_remove ) {
+			$user->removeGroup( $group_to_remove );
+		}
+		foreach ( array_diff( $new_oidc_groups, $old_oidc_groups ) as $group_to_add ) {
+			wfDebugLog("OpenIDConnect", "Adding group: " . $group_to_add);
+			$user->addGroup( $group_to_add );
+		}
+	}
+
+	private static function getOIDCGroups( User $user ) {
+		$accessToken = self::getAccessToken( $user );
+		if ( $accessToken === null ) {
+			wfDebugLog( 'OpenID Connect', 'No token found for user' . PHP_EOL );
+			return [];
+		}
+		$config = self::issuerConfig( $accessToken->iss );
+		if ( $config === null ) {
+			wfDebugLog( 'OpenID Connect', "No config found for issuer '$accessToken->iss'" . PHP_EOL );
+			return [];
+		}
+		$new_oidc_groups = [];
+		foreach ( [ 'global_roles', 'wiki_roles' ] as $role_config ) {
+			$roleProperty = self::getNestedPropertyAsArray( $config, [ $role_config, 'property' ] );
+			if ( empty( $roleProperty ) ) {
+				continue;
+			}
+			$intermediatePrefixes = ( self::getNestedPropertyAsArray( $config, [ $role_config, 'prefix' ] )
+				?: [ '' ] );
+			foreach ( self::getNestedPropertyAsArray( $accessToken, $roleProperty ) as $role ) {
+				foreach ( $intermediatePrefixes as $prefix ) {
+					$new_oidc_groups[] = self::OIDC_GROUP_PREFIX . $prefix . $role;
+				}
+			}
+		}
+		$new_oidc_groups = array_unique( $new_oidc_groups );
+		return $new_oidc_groups;
+	}
+
+	private static function getNestedPropertyAsArray( $obj, array $properties ) {
+		if ( $obj === null ) {
+			return [];
+		}
+		while ( !empty( $properties ) ) {
+			$property = array_shift( $properties );
+			if ( is_array( $obj ) ) {
+				if ( !array_key_exists( $property, $obj ) ) {
+					return [];
+				}
+				$obj = $obj[$property];
+			} else {
+				if ( !property_exists( $obj, $property ) ) {
+					return [];
+				}
+				$obj = $obj->$property;
+			}
+		}
+		return is_array( $obj ) ? $obj : [ $obj ];
+	}
+
+	private static function issuerConfig( $iss ) {
+		return isset( $GLOBALS['wgOpenIDConnect_Config'][$iss] )
+			? $GLOBALS['wgOpenIDConnect_Config'][$iss]
+			: null;
+	}
+
+	private static function getAccessToken( User $user ) {
+		$authManager = Authmanager::singleton();
+		$accessToken = $authManager->getAuthenticationSessionData(
+			self::OIDC_ACCESSTOKEN_SESSION_KEY);
+		if ( $accessToken === null ) {
+			return null;
+		}
+		list( $userId ) = self::findUser( $accessToken->sub, $accessToken->iss );
+		if ( $userId != $user->getId() ) {
+			return null;
+		}
+		return $accessToken;
+	}
+
+	private static function findUser( $subject, $issuer ) {
+		$dbr = wfGetDB( DB_REPLICA );
+		$row = $dbr->selectRow( 'user',
+			[   'user_id',
+				'user_name'
+			],
+			[
+				'subject' => $subject,
+				'issuer' => $issuer
+			], __METHOD__
+		);
+		if ( $row === false ) {
+			return [ null, null ];
+		} else {
+			return [ $row->user_id, $row->user_name ];
+		}
 	}
 
 	private static function getName( $subject, $issuer ) {
